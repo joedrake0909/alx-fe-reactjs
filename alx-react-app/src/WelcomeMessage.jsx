bfda407c377b276d698b8e0d528402f781891a7d
Joseph_Bio <josephbiossey03@gmail.com>
2026-02-09 13:39:49 +0000

    Modify JSX in a Pre-built React Component

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hey Everyone,This is Joe, I am learning React at ALX!</h1>
+            <p>I am learning about JSX</p>
+            <p>This is a simple JSX component.</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
