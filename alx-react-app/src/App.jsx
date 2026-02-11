6b4ce2a6aa4d08325ac316dec7983e18208e0c7d
Joseph_Bio <josephbiossey03@gmail.com>
2026-02-11 09:41:31 +0000

    Create Specific Components in a React Project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,26 +14,6 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
     
 
@@ -45,4 +21,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
