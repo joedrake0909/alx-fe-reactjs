ffe34ca1bf1c2cdcd9871e1e3c4f235f6a51156f
Joseph_Bio <josephbiossey03@gmail.com>
2026-02-21 10:15:57 +0000

    Added span and solid property to the UserProfile comp

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,11 +1,10 @@
 const UserProfile = (props) => {
     return (
-        <div style={{ backgroundColor: 'lightblue', padding: '20px', margin: '10px', borderRadius: '20px', textAlign: 'center', fontFamily: 'Arial, sans-serif', color: 'darkblue', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', }}>
-            <h2 style={{ fontWeight: '90px', margin: '0px' }}>{props.name}</h2>
-            <p style={{ fontSize: '20px' }}>Age: {props.age}</p>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h2 style={{ color: 'blue' }}>{props.name}</h2>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
         </div>
-
     );
 
 };
